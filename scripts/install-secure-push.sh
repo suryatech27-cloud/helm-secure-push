82bf8107018bf92f4d4160ca4cab3f4b45265501
suryatech27-cloud <sunil.techno27@gmail.com>
2021-06-25 17:09:28 +0530

    Install-script changes for linux/amd64

--- scripts/install-secure-push.sh
+++ scripts/install-secure-push.sh
@@ -32,13 +32,9 @@ if [ $OS == "windows" ]; then
 
         cd $DIR'/helm-secure-push/bin'; env GOOS=windows GOARCH=amd64 go build ../cmd/securepush
         cd 'C:/Windows/System32'; setx path "$DIR\helm-secure-push\bin"
-<<<<<<< HEAD
 else if [ $OS == 'linux']; then
   sudo echo 'export PATH=$PATH:$DIR/helm-secure-push/bin' >> ~/.bashrc
   sudo source ~/.bashrc
-=======
-else
->>>>>>> cfd7072471502df43d1dc569f1dc7280b8a6a40d
   echo "Installed Successfully Done."
 else 
   echo "Environmnet setup failed due to other OS implementarion".
