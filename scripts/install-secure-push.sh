0b00aafb1d450d3c07f83deb85b7971597b86b5c
suryatech27-cloud <sunil.techno27@gmail.com>
2021-07-02 14:25:06 +0530

    Install-script changes

--- scripts/install-secure-push.sh
+++ scripts/install-secure-push.sh
@@ -30,13 +30,15 @@ if [ $OS == "windows" ]; then
         cd $DIR'/helm-secure-push/bin'; rm 'securepush';
         #echo $PUSH_BIN_PATH
 
-        cd $DIR'/helm-secure-push/bin'; env GOOS=windows GOARCH=amd64 go build ../cmd/securepush
+        #cd $DIR'/helm-secure-push/bin'; env GOOS=windows GOARCH=amd64 go build ../cmd/securepush
         cd 'C:/Windows/System32'; setx path "$DIR\helm-secure-push\bin"
 elif [ $OS == "linux" ]; then
+  cd $DIR'/helm-secure-push/bin'; rm 'securepush.exe';
   echo 'export PATH=$PATH:'$DIR'/helm-secure-push/bin' >> ~/.bashrc
   source ~/.bashrc
   echo "Installed Successfully Done."
 else 
+  cd $DIR'/helm-secure-push/bin'; rm 'securepush.exe';
   echo 'export PATH=$PATH:'$DIR'/helm-secure-push/bin' >> ~/.bashrc
   source ~/.bashrc
   echo "Installed Successfully Done."
